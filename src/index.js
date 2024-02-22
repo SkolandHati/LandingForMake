4c1ca806846fd401d350197a9d5e2afcb1905aeb
SkolandHati <vlad.star.01@inbox.ru>
2024-02-22 17:18:41 +0300

    update project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import {addHoverEffect} from "./modules/hover_effect";
 import "./style/scss/main.scss"
 
 let isOpen = false
 let isDrop = false
 let accordion = document.querySelector(".accordion")
-
 let dropdown = document.querySelector(".list__one")
-
+let sBtn_text = dropdown.querySelector(".text_md");
+console.log(sBtn_text)
 accordion.addEventListener('click', () => {
     if (!isOpen) {
         accordion.classList.add('accordion_open')
@@ -20,15 +21,33 @@ accordion.addEventListener('click', () => {
 dropdown.addEventListener('click', () => {
     let dropButton = dropdown.querySelector(".dropdown")
     let menu = dropdown.querySelector(".dropdown-menu")
+    let option = menu.querySelectorAll('.text_md')
+    // addHoverEffect(option, 'color: white;\n' +
+    //     '              background-color: #3E29E3;')
     if (!isDrop) {
+        menu.classList.remove('dropdown-menu__close')
         menu.classList.add('dropdown-menu__active')
         dropButton.classList.add('dropdown_down')
+        option.forEach((text_md) => {
+            text_md.addEventListener('click', () => {
+                let text = sBtn_text.querySelector('.sBtn_text')
+                text.innerText = text_md.innerText;
+                text_md.style.color = '#3E29E3'
+            })
+        })
         isDrop = true
     }
     else {
         menu.classList.remove('dropdown-menu__active')
+        menu.classList.add('dropdown-menu__close')
         dropButton.classList.remove('dropdown_down')
         isDrop = false
+        // option.forEach((text_md) => {
+        //     let text = sBtn_text.querySelector('.sBtn_text')
+        //     if (text.innerText !== text_md.innerText) {
+        //         text_md.style.color = '#1B1B1B'
+        //     }
+        // })
     }
 })
 
--- src/modules/hover_effect.js
+++ src/modules/hover_effect.js
@@ -1,11 +1,8 @@
-export function addHoverEffect(status, className, hoverStyle_one) {
-    if (!status){
-        return
-    }
-    let elements = document.getElementsByClassName(className);
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].onmouseover = function() { this.style.cssText = hoverStyle_one; };
-        elements[i].onmouseout = function() { this.style.cssText = ''; };
+export function addHoverEffect(list, hoverStyle ) {
+
+    for (let i = 0; i < list.length; i++) {
+        list[i].onmouseover = function() { this.style.cssText = hoverStyle; };
+        list[i].onmouseout = function() { this.style.cssText = 'background-color:: white;'; };
     }
 }
 
