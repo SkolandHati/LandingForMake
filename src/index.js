e732e40af6fb4fec3a6ac557a3852ccec92a26c5
SkolandHati <vlad.star.01@inbox.ru>
2024-02-23 12:05:52 +0300

    update project

--- src/index.js
+++ src/index.js
@@ -1,46 +1,49 @@
 import "./style/scss/main.scss"
 
-let isOpen = false
-let isDrop = false
+let isAccordionOpen = false
 let accordion = document.querySelector(".accordion")
-let dropdown = document.querySelector(".list__one")
-let sBtn_text = dropdown.querySelector(".text_md");
-console.log(sBtn_text)
 accordion.addEventListener('click', () => {
-    if (!isOpen) {
+    if (!isAccordionOpen) {
         accordion.classList.add('accordion_open')
-        isOpen = true
+        isAccordionOpen = true
     }
     else {
         accordion.classList.remove('accordion_open')
-        isOpen = false
+        isAccordionOpen = false
     }
 })
 
+let isDropOpen = false
+let dropdown = document.querySelector(".list__one")
+let dropdownPlaceholder = dropdown.querySelector(".dropdown__selected")
+let selectedOption = null;
 dropdown.addEventListener('click', () => {
     let dropButton = dropdown.querySelector(".dropdown")
     let menu = dropdown.querySelector(".dropdown-menu")
-    let option = menu.querySelectorAll('.text_md')
+    let options = menu.querySelectorAll('.text_md')
 
-    if (!isDrop) {
-        menu.classList.remove('dropdown-menu__close')
-        menu.classList.add('dropdown-menu__active')
+    if (!isDropOpen) {
+        menu.classList.remove('dropdown-menu_close')
+        menu.classList.add('dropdown-menu_active')
         dropButton.classList.add('dropdown_down')
-        option.forEach((text_md) => {
-            text_md.addEventListener('click', () => {
-                let text = sBtn_text.querySelector('.sBtn_text')
-                text.innerText = text_md.innerText;
-                text_md.style.color = '#3E29E3'
+        options.forEach((option) => {
+            option.addEventListener('click', () => {
+                selectedOption?.classList.remove('dropdown__option_active')
+                dropdownPlaceholder.innerText = option.innerText
+                option.classList.add('dropdown__option_active');
+                selectedOption = option;
             })
         })
-        isDrop = true
+        isDropOpen = true
     }
     else {
-        menu.classList.remove('dropdown-menu__active')
-        menu.classList.add('dropdown-menu__close')
+        menu.classList.remove('dropdown-menu_active')
+        menu.classList.add('dropdown-menu_close')
         dropButton.classList.remove('dropdown_down')
-        isDrop = false
+        isDropOpen = false
     }
 })
+
+
 
 
