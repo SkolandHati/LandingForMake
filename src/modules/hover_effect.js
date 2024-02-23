219d7459e4aa12c466e19ead4490233f9256c95f
SkolandHati <vlad.star.01@inbox.ru>
2024-02-23 15:38:12 +0300

    update project

--- src/modules/hover_effect.js
+++
@@ -1,8 +0,0 @@
-export function addHoverEffect(list, hoverStyle ) {
-
-    for (let i = 0; i < list.length; i++) {
-        list[i].onmouseover = function() { this.style.cssText = hoverStyle; };
-        list[i].onmouseout = function() { this.style.cssText = 'background-color:: white;'; };
-    }
-}
-
